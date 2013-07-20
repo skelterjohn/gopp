293a48bc60b505f276279675285165253f5fe4e0
John Asmuth <jasmuth@gmail.com>
2013-07-20 02:02:41 -0400

    tokenizing works

--- gopp.go
+++ gopp.go
@@ -1,8 +1,49 @@
 package gopp
 
+import (
+	"regexp"
+	"sort"
+)
+
+const REGEXP_PREFIX = `^(?:\s)*`
+
 type Grammar struct {
-	Rules []*Rule
+	Rules   []*Rule
 	Symbols []*Symbol
+}
+
+func (g *Grammar) CollectLiterals(literals map[string]bool) {
+	for _, rule := range g.Rules {
+		rule.CollectLiterals(literals)
+	}
+	return
+}
+
+func (g *Grammar) TokenREs() (res []*regexp.Regexp, err error) {
+	// first get all the literals, and sort them longest first (so smaller ones don't eat larger ones).
+	literals := map[string]bool{}
+	g.CollectLiterals(literals)
+	sortedLiterals := literalSorter{}
+	for literal := range literals {
+		sortedLiterals = append(sortedLiterals, literal)
+	}
+	sort.Sort(sortedLiterals)
+	for _, literal := range sortedLiterals {
+		re, err := regexp.Compile(REGEXP_PREFIX + "(" + regexp.QuoteMeta(literal) + ")")
+		if err != nil {
+			panic("regexp.QuoteMeta returned something that didn't compile")
+		}
+		res = append(res, re)
+	}
+	for _, symbol := range g.Symbols {
+		var re *regexp.Regexp
+		re, err = regexp.Compile(REGEXP_PREFIX + symbol.Pattern)
+		if err != nil {
+			return
+		}
+		res = append(res, re)
+	}
+	return
 }
 
 type Rule struct {
@@ -10,8 +51,13 @@ type Rule struct {
 	Expr *Expr
 }
 
+func (r *Rule) CollectLiterals(literals map[string]bool) {
+	r.Expr.CollectLiterals(literals)
+	return
+}
+
 type Symbol struct {
-	Name string
+	Name    string
 	Pattern string
 }
 
@@ -19,127 +65,31 @@ type Expr struct {
 	Terms []*Term
 }
 
-type Term struct {
-	Operator string
-	Term *Term
-	Expr *Expr
-	Field string
-	Name string
-	Literal string
+func (e *Expr) CollectLiterals(literals map[string]bool) {
+	for _, term := range e.Terms {
+		term.CollectLiterals(literals)
+	}
+	return
 }
 
-var ByHandGrammar = Grammar{
-	Rules: []*Rule{
-		&Rule{ // Grammar => Rules=<<Rule>>+ Symbols=<<Symbol>>+
-			Name: "Grammar",
-			Expr: &Expr{ // Rules=<<Rule>>+ Symbols=<<Symbol>>+
-				Terms: []*Term{
-					&Term{ // Rules=<<Rule>>+
-						Operator: "=",
-						Field: "Rules",
-						Term: &Term{ // <<Rule>>+
-							Operator: "+",
-							Term: &Term{ // <<Rule>>
-								Operator: "<<",
-								Name: "Rule",
-							},
-						},
-					},
-					&Term{ // Symbols=<<Symbol>>+
-						Operator: "=",
-						Field: "Symbols",
-						Term: &Term{ // <<Symbol>>+
-							Operator: "+",
-							Term: &Term{ // <<Symbol>>
-								Operator: "<<",
-								Name: "Symbol",
-							},
-						},
-					},
-				},
-			},
-		},
-		&Rule{ // Rule => Name=<identifier> '=>' Expr=<<Expr>> '\n'+
-			Name: "Rule",
-			Expr: &Expr{ // Name=<identifier> '=>' Expr=<<Expr>> '\n'+
-				Terms: []*Term{
-					&Term{ // Name=<identifier>
-						Operator: "=",
-						Field: "Name",
-						Term: &Term { // <identifier>
-							Operator: "<",
-							Name: "identifier",
-						},
-					},
-					&Term{ // '=>'
-						Literal: "=>",
-					},
-					&Term{ // Expr=<<Expr>>
-						Operator: "=",
-						Field: "Expr",
-						Term: &Term { // <<Expr>>
-							Operator: "<<",
-							Name: "Expr",
-						},
-					},
-					&Term{ // '\n'+
-						Operator: "+",
-						Term: &Term{ // '\n'
-							Literal: "\n",
-						},
-					},
-				},
-			},
-		},
-		&Rule{ // Symbol => Name=<identifier> '=' Pattern=<regexp> '\n'+
-			Name: "Symbol",
-			Expr: &Expr{ // Name=<identifier> '=' Pattern=<regexp> '\n'+
-				Terms: []*Term{
-					&Term{ // Name=<identifier>
-						Operator: "=",
-						Field: "Name",
-						Term: &Term { // <identifier>
-							Operator: "<",
-							Name: "identifier",
-						},
-					},
-					&Term{ // '='
-						Literal: "=",
-					},
-					&Term{ // Pattern=<regexp>
-						Operator: "=",
-						Field: "Pattern",
-						Term: &Term { // <regexp>
-							Operator: "<",
-							Name: "regexp",
-						},
-					},
-					&Term{ // '\n'+
-						Operator: "+",
-						Term: &Term{ // '\n'
-							Literal: "\n",
-						},
-					},
-				},
-			},
-		},
-		&Rule{ // Expr => Terms=<<Term>>+
-			Name: "Expr",
-			Expr: &Expr{
-				Terms: []*Term{
-					&Term{ // Terms=<<Term>>+
-						Operator: "=",
-						Field: "Terms",
-						Term: &Term{ // <<Term>>+
-							Operator: "+",
-							Term: &Term{ // <<Term>>
-								Operator: "<<",
-								Name: "Term",
-							},
-						},
-					},
-				},
-			},
-		},
-	},
+type Term struct {
+	Operator string
+	Term     *Term
+	Expr     *Expr
+	Field    string
+	Name     string
+	Literal  string
+}
+
+func (t *Term) CollectLiterals(literals map[string]bool) {
+	if t.Literal != "" {
+		literals[t.Literal] = true
+	}
+	if t.Expr != nil {
+		t.Expr.CollectLiterals(literals)
+	}
+	if t.Term != nil {
+		t.Term.CollectLiterals(literals)
+	}
+	return
 }
