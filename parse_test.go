c8062d2c606a9bb6f387db5a823ed5015ac7d86f
John Asmuth <jasmuth@gmail.com>
2013-07-29 14:51:37 -0700

    illustrative test for math eqn stuff

--- parse_test.go
+++ parse_test.go
@@ -10,33 +10,55 @@ import (
 
 const mathgopp = `
 Eqn => {field=Left} <<Expr>> '=' {field=Right} <<Expr>> '\n'
-Expr => {field=First} <<Term>> '+' {field=Second} <<Term>>
-Expr => {field=First} <<Term>>
-Term => {field=First} <<Factor>> '*' {field=Second} <<Factor>>
-Term => {field=First} <<Factor>>
+Expr => {type=MathSum} {field=First} <<Term>> '+' {field=Second} <<Term>>
+Expr => <Term>
+Term => {type=MathProduct} {field=First} <<Factor>> '*' {field=Second} <<Factor>>
+Term => <Factor>
 Factor => {type=MathExprFactor} '(' {field=Expr} <<Expr>> ')'
 Factor => {type=MathNumberFactor} {field=Number} <number>
 number = /(\d+)/
 `
 
 type MathEqn struct {
-	Left, Right MathExpr
+	Left, Right interface{}
 }
 
-type MathExpr struct {
-	First, Second MathTerm
+func (e MathEqn) String() string {
+	return fmt.Sprintf("%s=%s", e.Left, e.Right)
 }
 
-type MathTerm struct {
+type MathSum struct {
 	First, Second interface{}
 }
+
+func (s MathSum) String() string {
+	return fmt.Sprintf("%s+%s", s.First, s.Second)
+}
+
+type MathProduct struct {
+	First, Second interface{}
+}
+
+func (p MathProduct) String() string {
+	return fmt.Sprintf("%s*%s", p.First, p.Second)
+}
+
 
 type MathExprFactor struct {
 	Expr
 }
 
+func (ef MathExprFactor) String() string {
+	return fmt.Sprintf("(%s)", ef.Expr)
+}
+
+
 type MathNumberFactor struct {
 	Number string
+}
+
+func (nf MathNumberFactor) String() string {
+	return nf.Number
 }
 
 func TestMath(t *testing.T) {
@@ -49,6 +71,8 @@ func TestMath(t *testing.T) {
 	}
 	df.RegisterType(MathExprFactor{})
 	df.RegisterType(MathNumberFactor{})
+	df.RegisterType(MathSum{})
+	df.RegisterType(MathProduct{})
 	dec := df.NewDecoder(strings.NewReader("5+1=6"))
 	var eqn MathEqn
 	err = dec.Decode(&eqn)
@@ -56,5 +80,7 @@ func TestMath(t *testing.T) {
 		t.Error(err)
 		return
 	}
-	fmt.Printf("%+v\n", eqn)
+	if eqn.String() != "5+1=6" {
+		t.Errorf("Expected %q, got %q.", "5+1=6", eqn)
+	}
 }
